654009167bfb7592911706e939b6beb3ddef487a
Lucio Villa <contact@luciovilla.com>
2014-06-24 16:21:39 -0500

    Single Standard post format created with related posts, social media icons, no ads, no footer

--- wp-content/themes/twentythirteen-child/author-bio.php
+++ wp-content/themes/twentythirteen-child/author-bio.php
@@ -23,11 +23,11 @@
 		?>
 	</div><!-- .author-avatar -->
 	<div class="author-description">
-		<h2 class="author-title">El Autor</h2>
+		<h2 class="author-title"><?php printf( __( '%s', 'twentythirteen' ), get_the_author() ); ?></h2>
 		<p class="author-bio">
 			<?php the_author_meta( 'description' ); ?>
 			<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-				<?php printf( __( 'Mas noticias de %s <span class="meta-nav">&rarr;</span>', 'twentythirteen' ), get_the_author() ); ?>
+				<?php printf( __( 'Mas noticas por %s <span class="meta-nav">&rarr;</span>', 'twentythirteen' ), get_the_author() ); ?>
 			</a>
 		</p>
 	</div><!-- .author-description -->
--- wp-content/themes/twentythirteen-child/header.php
+++ wp-content/themes/twentythirteen-child/header.php
@@ -33,17 +33,18 @@
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
-			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</a>
+			
 
 			<div id="navbar" class="navbar">
 				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
+					
+					<h2 class="menu-toggle"><?php _e( 'Menú', 'twentythirteen' ); ?></h2>
+					<a href="http://www.vivelohoy.com"><img class="nav-logo" src="http://www.vivelohoy.com/wp-content/themes/vivelohoy/assets/images/vivelohoy_logo.png"></a>
 					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+						
 					<?php get_search_form(); ?>
+					
 				</nav><!-- #site-navigation -->
 			</div><!-- #navbar -->
 		</header><!-- #masthead -->
