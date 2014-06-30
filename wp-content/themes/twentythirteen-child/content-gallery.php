f6bd9d3e222538fcbfee8cf120acc30f563b9de4
Lucio Villa <contact@luciovilla.com>
2014-06-30 16:03:40 -0500

    Modified the entry-meta in gallery post type

--- wp-content/themes/twentythirteen-child/content-gallery.php
+++ wp-content/themes/twentythirteen-child/content-gallery.php
@@ -11,12 +11,19 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php if ( is_single() ) : ?>
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<center><h1 class="entry-title"><?php the_title(); ?></h1></center>
 		<?php else : ?>
 		<h1 class="entry-title">
 			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
 		</h1>
 		<?php endif; // is_single() ?>
+		<div class="entry-meta-hoy">
+		<center>
+			<p>
+				<?php the_category(', ') ?> Por <?php the_author_posts_link(); ?> <?php the_time('m/j/y g:i A') ?>
+		</p>
+		<?php edit_post_link( __( 'Editar', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
+		</center></div>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -29,17 +36,14 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-meta">
-		<?php twentythirteen_entry_meta(); ?>
+		
 
 		<?php if ( comments_open() && ! is_single() ) : ?>
 		<span class="comments-link">
 			<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'twentythirteen' ) . '</span>', __( 'One comment so far', 'twentythirteen' ), __( 'View all % comments', 'twentythirteen' ) ); ?>
 		</span><!-- .comments-link -->
 		<?php endif; // comments_open() ?>
-		<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
-
-		<?php if ( is_single() && get_the_author_meta( 'description' ) && is_multi_author() ) : ?>
-			<?php get_template_part( 'author-bio' ); ?>
-		<?php endif; ?>
+		
+		
 	</footer><!-- .entry-meta -->
 </article><!-- #post -->
