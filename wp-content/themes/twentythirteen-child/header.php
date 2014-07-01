74ff52d984ebd632a2d6d50750fa55941009f17d
Nick Bennett <nbennett@vivelohoy.com>
2014-07-01 15:50:18 -0500

    Add Google Analytics (#8).

--- wp-content/themes/twentythirteen-child/header.php
+++ wp-content/themes/twentythirteen-child/header.php
@@ -28,6 +28,7 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
 	<?php wp_head(); ?>
+	<?php include_once("analyticstracking.php") ?>
 </head>
 
 <body <?php body_class(); ?>>
