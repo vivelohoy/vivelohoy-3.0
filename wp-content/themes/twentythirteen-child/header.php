50922abce0ab34a57ba292da32e8e2431ab5ebf3
Nick Bennett <nbennett@vivelohoy.com>
2014-07-01 16:43:27 -0500

    Change location of Google Analytics script to just inside body tag.

--- wp-content/themes/twentythirteen-child/header.php
+++ wp-content/themes/twentythirteen-child/header.php
@@ -28,10 +28,10 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
 	<?php wp_head(); ?>
-	<?php include_once("analyticstracking.php") ?>
 </head>
 
 <body <?php body_class(); ?>>
+	<?php include_once("analyticstracking.php") ?>
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
 			
