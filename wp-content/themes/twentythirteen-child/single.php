ba10f26e0d5b9a712b1fc8771e0fadf1ab3c48a7
Lucio Villa <contact@luciovilla.com>
2014-07-02 11:50:43 -0500

    Added if statement to disable top leaderboard in gallery post type page (#34).

--- wp-content/themes/twentythirteen-child/single.php
+++ wp-content/themes/twentythirteen-child/single.php
@@ -14,9 +14,11 @@ get_header(); ?>
 	<div id="page" class="hfeed site">
 		
 <!-- TOP LEADERBOARD AD -->	
+	<?php if ( get_post_format() === false ) : ?>
 	<div id="topleaderboard-post">
 	<iframe id="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=%s;ord=%s" height="90" width="728" vspace="0" hspace="0" marginheight="0" marginwidth="0" align="center" frameborder="0" scrolling="no" src="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=http://www.vivelohoy.com/;ord=86950313"></iframe>
 	</div>
+<?php endif; // get_post_format() ?>
 <!-- TOP LEADERBOARD AD -->
 		
 
