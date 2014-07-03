2907d4016aeb8eba059f308e133556b53c2832c8
Nick Bennett <nbennett@vivelohoy.com>
2014-07-03 15:06:44 -0500

    Override gallery shortcode to display images full size with no link.

--- wp-content/themes/twentythirteen-child/functions.php
+++ wp-content/themes/twentythirteen-child/functions.php
@@ -53,98 +53,12 @@ function childtheme_formats(){
 remove_shortcode('gallery', 'gallery_shortcode');
 add_shortcode('gallery', 'custom_gallery');
 
+
 function custom_gallery($attr) {
-	$post = get_post();
-
-	static $instance = 0;
-	$instance++;
-
-	# hard-coding these values so that they can't be broken
-	
+	// hard-coding these values so that they can't be broken
 	$attr['columns'] = 1;
 	$attr['size'] = 'full';
 	$attr['link'] = 'none';
-
-	$attr['orderby'] = 'post__in';
-	$attr['include'] = $attr['ids'];		
-
-	#Allow plugins/themes to override the default gallery template.
-	$output = apply_filters('post_gallery', '', $attr);
-	
-	if ( $output != '' )
-		return $output;
-
-	# We're trusting author input, so let's at least make sure it looks like a valid orderby statement
-	if ( isset( $attr['orderby'] ) ) {
-		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
-		if ( !$attr['orderby'] )
-			unset( $attr['orderby'] );
-	}
-
-	extract(shortcode_atts(array(
-		'order'      => 'ASC',
-		'orderby'    => 'menu_order ID',
-		'id'         => $post->ID,
-		'itemtag'    => 'div',
-		'icontag'    => 'div',
-		'captiontag' => 'p',
-		'columns'    => 3,
-		'size'       => 'thumbnail',
-		'include'    => '',
-		'exclude'    => ''
-	), $attr));
-
-	$id = intval($id);
-	if ( 'RAND' == $order )
-		$orderby = 'none';
-
-	if ( !empty($include) ) {
-		$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
-
-		$attachments = array();
-		foreach ( $_attachments as $key => $val ) {
-			$attachments[$val->ID] = $_attachments[$key];
-		}
-	} elseif ( !empty($exclude) ) {
-		$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
-	} else {
-		$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
-	}
-
-	if ( empty($attachments) )
-		return '';
-
-	$gallery_style = $gallery_div = '';
-
-	if ( apply_filters( 'use_default_gallery_style', true ) )
-		$gallery_style = "<!-- see gallery_shortcode() in functions.php -->";
-	
-	$gallery_div = "<div id='homepage-gallery-wrap' class='gallery gallery-columns-1 gallery-size-full link-none	'>";
-	
-	$output = apply_filters( 'gallery_style', $gallery_style . "\n\t\t" . $gallery_div );
-	
-	foreach ( $attachments as $id => $attachment ) {
-		$link = wp_get_attachment_link($id, 'full');
-
-		$output .= "<div class='homepage-gallery-item'>";
-		$output .= "
-			<div class='homepage-gallery-icon'>
-				$link
-			</div>";
-		if ( $captiontag && trim($attachment->post_excerpt) ) {
-			$output .= "
-				<p style='font-size: .8125rem; font-style: italic; 
-				margin: 5px 0 24px; font-family: Helvetica, Arial, sans-serif;'>
-				" . wptexturize($attachment->post_excerpt) . "
-				</p>";
-		}
-		$output .= "</div>";
-	}
-
-	$output .= "</div>\n";
-
-	return $output;
+	// Let the normal gallery shortcode handler do the rest
+	return gallery_shortcode( $attr );
 }
-
-// Removing image attachment links
-// update_option('image_default_link_type','none');
