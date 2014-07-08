34159e974cf9a4845cce3785918e24f8ee39c0e5
Nick Bennett <nbennett@vivelohoy.com>
2014-07-08 09:58:53 -0500

    Make leaderboard ads appear after every 2nd image in gallery.

--- wp-content/themes/twentythirteen-child/content-gallery.php
+++ wp-content/themes/twentythirteen-child/content-gallery.php
@@ -71,7 +71,7 @@
 (function($) {
 	leaderboard_code = $('<div class="gallery-leaderboard"><iframe id="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=%s;ord=%s" height="90" width="728" vspace="0" hspace="0" marginheight="0" marginwidth="0" align="center" frameborder="0" scrolling="no" src="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=http://www.vivelohoy.com/;ord=86950313"></iframe></div>');
 	$(document).ready(function() {
-		$('.gallery-item:not(:last)').after(leaderboard_code);
+		$('.gallery-item:nth-child(2n)').after(leaderboard_code);
 	});
 })(jQuery);
 </script>
