d9563b86c8fc4ec3fb283d91a379701fc5cceec7
Lucio Villa <contact@luciovilla.com>
2014-07-09 17:30:23 -0500

    Updated author page headline color and image links.

--- wp-content/themes/twentythirteen-child/author.php
+++ wp-content/themes/twentythirteen-child/author.php
@@ -87,11 +87,11 @@ get_header(); ?>
 				<?php while (have_posts()) : the_post(); ?>
 				<?php if ('gallery' === get_post_format($post->ID)) : ?>
 					<div class="excerpt-post clearfix">
-						<div style="float: left"><?php $image=wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail' );
+						<div style="float: left"><a href="<?php the_permalink() ?>" rel="bookmark" accesskey="s"><?php $image=wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail' );
 							$imgurl=$image[0];
-						 	?><img style="padding-right:10px" src="<?php echo $imgurl;?>">
+						 	?><img style="padding-right:10px" src="<?php echo $imgurl;?>"></a>
 						</div>
-						<div style="margin: 0 20px 0 0">
+						<div class="author-meta" style="margin: 0 20px 0 0">
 							<h3 id="post-<?php the_ID(); ?>" style="display: inline">
 								<a href="<?php the_permalink() ?>" rel="bookmark" accesskey="s"><?php the_title(); ?></a>
 							</h3><br>
@@ -108,15 +108,15 @@ get_header(); ?>
 				<?php else : ?>
 					<?php if ( $post->post_excerpt ) : // If there is an explicitly defined excerpt ?>
 					<div class="excerpt-post clearfix">
-						<div style="float: left"><?php $image=wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail' );
+						<div style="float: left"><a href="<?php the_permalink() ?>" rel="bookmark" accesskey="s"><?php $image=wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail' );
 							$imgurl=$image[0];
-						 	?><img style="padding-right:10px" src="<?php echo $imgurl;?>">
+						 	?><img style="padding-right:10px" src="<?php echo $imgurl;?>"></a>
 						</div>
-						<div style="margin: 0 20px 0 0">
+						<div class="author-meta" style="margin: 0 20px 0 0">
 							<h3 id="post-<?php the_ID(); ?>" style="display: inline">
 								<a href="<?php the_permalink() ?>" rel="bookmark" accesskey="s"><?php the_title(); ?></a>
 							</h3><br>
-							<h6 style="display: inline; color: #808080;font-weight: 400;letter-spacing: 0.06em;}">
+							<h6 class="author-cat" style="display: inline; color: #808080;font-weight: 400;letter-spacing: 0.06em;}">
 								<?php $category = get_the_category(); 
 								if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>';}?>
 							</h6>
