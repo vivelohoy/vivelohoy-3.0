cfd061019c2a346efc9bdda8c3c71aea8018d895
Nick Bennett <nbennett@vivelohoy.com>
2014-07-11 11:16:26 -0500

    Moved custom post loop from author.php to custom-loop.php.

---
+++ wp-content/themes/twentythirteen-child/custom-loop.php
@@ -0,0 +1,51 @@
+<?php while (have_posts()) : the_post(); ?>
+<?php if ('gallery' === get_post_format($post->ID)) : ?>
+    <div class="excerpt-post clearfix">
+        <div style="float: left"><a href="<?php the_permalink() ?>" rel="bookmark" accesskey="s"><?php $image=wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+            $imgurl=$image[0];
+            ?><img style="padding-right:10px; width:368px" src="<?php echo $imgurl;?>"></a>
+        </div>
+        <div class="author-meta" style="margin: 0 20px 0 0">
+            <h3 id="post-<?php the_ID(); ?>" style="display: inline">
+                <a href="<?php the_permalink() ?>" rel="bookmark" accesskey="s"><?php the_title(); ?></a>
+            </h3><br>
+            <h6 style="display: inline; color: #808080;font-weight: 400;letter-spacing: 0.06em;}">
+                <?php $category = get_the_category(); 
+                if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>';}?>
+            </h6>
+            <br>
+            
+                <?php the_excerpt(); ?>
+            
+        </div>
+    </div>
+<?php else : // if get_post_format() is not 'gallery' ?>
+    <?php if ( $post->post_excerpt ) : // If there is an explicitly defined excerpt ?>
+    <div class="excerpt-post clearfix">
+        <div style="float: left"><a href="<?php the_permalink() ?>" rel="bookmark" accesskey="s"><?php $image=wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+            $imgurl=$image[0];
+            ?><img style="padding-right:10px; width:368px" src="<?php echo $imgurl;?>"></a>
+        </div>
+        <div class="author-meta" style="margin: 0 20px 0 0">
+            <h3 id="post-<?php the_ID(); ?>" style="display: inline">
+                <a href="<?php the_permalink() ?>" rel="bookmark" accesskey="s"><?php the_title(); ?></a>
+            </h3><br>
+            <h6 class="author-cat" style="display: inline; color: #808080;font-weight: 400;letter-spacing: 0.06em;}">
+                <?php $category = get_the_category(); 
+                if($category[0]){echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>';}?>
+            </h6>
+            <br>
+            
+                <?php the_excerpt(); ?>
+            
+        </div>
+    </div>
+    <?php else : // If there is not an explictly defined excerpt ?>
+    <div class="excerpt-post clearfix">
+        <h2 id="post-<?php the_ID(); ?>">
+            <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+        </h2>
+    </div><!-- end of excerpt-post -->
+    <?php endif; // End the excerpt vs. content "if" statement ?>
+<?php endif; // End gallery vs standard statement ?>
+<?php endwhile; // End while ( have_posts() ) ?>
