fb1c345228cfa52853c745023b0d1bc650723d36
Nick Bennett <nbennett@vivelohoy.com>
2014-07-11 12:54:28 -0500

    Add home.php to override twentythirteen's index.php, using custom-loop.php for post loop.

---
+++ wp-content/themes/twentythirteen-child/home.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * The homepage template
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* The loop */ ?>
+			<?php include_once("custom-loop.php") ?>
+
+			<?php twentythirteen_paging_nav(); ?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
