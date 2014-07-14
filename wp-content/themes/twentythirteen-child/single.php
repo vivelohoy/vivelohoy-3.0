ef7c0cf096e7514da0f05251f17a03d4f3086d9d
Lucio Villa <contact@luciovilla.com>
2014-07-14 16:37:03 -0500

    Activated page navigation in posts.

--- wp-content/themes/twentythirteen-child/single.php
+++ wp-content/themes/twentythirteen-child/single.php
@@ -27,6 +27,7 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'content', get_post_format() ); ?>	
 			<?php endwhile; ?>
+			<?php twentythirteen_post_nav(); ?>
 			
 
 <!-- BOTTOM LEADERBOARD AD -->	
