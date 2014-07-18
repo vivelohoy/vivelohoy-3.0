47c645fe1b875feac661bb76e56f3a5ff8884bd1
Nick Bennett <nbennett@vivelohoy.com>
2014-07-18 16:16:19 -0500

    Change image.php to use translated strings. #98

--- wp-content/themes/twentythirteen-child/image.php
+++ wp-content/themes/twentythirteen-child/image.php
@@ -16,8 +16,8 @@ get_header(); ?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class( 'image-attachment' ); ?>>
 				<div class="entry-content">
 					<nav id="image-navigation" class="navigation image-navigation" role="navigation">
-						<span class="nav-previous"><?php previous_image_link( false, __( '<span class="meta-nav">&larr;</span> Anterior', 'twentythirteen' ) ); ?></span>
-						<span class="nav-next"><?php next_image_link( false, __( 'Siguiente <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); ?></span>
+						<span class="nav-previous"><?php previous_image_link( false, __( '<span class="meta-nav">&larr;</span> Previous', 'twentythirteen-child' ) ); ?></span>
+						<span class="nav-next"><?php next_image_link( false, __( 'Next <span class="meta-nav">&rarr;</span>', 'twentythirteen-child' ) ); ?></span>
 					</nav><!-- #image-navigation -->
 
 					<div class="entry-attachment">
@@ -35,7 +35,7 @@ get_header(); ?>
 					<?php if ( ! empty( $post->post_content ) ) : ?>
 					<div class="entry-description">
 						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentythirteen' ), 'after' => '</div>' ) ); ?>
+						<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentythirteen-child' ), 'after' => '</div>' ) ); ?>
 					</div><!-- .entry-description -->
 					<?php endif; ?>
 				</div><!-- .entry-content -->
