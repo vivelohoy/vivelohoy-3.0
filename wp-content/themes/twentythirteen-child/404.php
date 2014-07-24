0229c4094bbc69259c8560c12809a9f04c69b0fc
Lucio Villa <contact@luciovilla.com>
2014-07-24 16:04:55 -0500

    Updated 404 page.

--- wp-content/themes/twentythirteen-child/404.php
+++ wp-content/themes/twentythirteen-child/404.php
@@ -8,25 +8,16 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-			<div>
-				<div class="page-content">
-					<h2><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentythirteen-child' ); ?></h2>
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen-child' ); ?></p>
-					<div>
-						<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/404.jpg" alt="Error Cuatro-Cero-Cuatro..." width="100%">
-					</div>
+</div><div style="margin: 30px auto">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/404.jpg" alt="Error Cuatro-Cero-Cuatro..." width="100%">
 					
-					<div class="error404-search">
+				<div class="page-content">
+					<h2 class="error404-search"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentythirteen-child' ); ?></h2>
+					<p style="margin: 0 0 5px 0"><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen-child' ); ?></p>
+					
 						<?php get_search_form(); ?>
-					</div>
+					
 				</div><!-- .page-content -->
-			</div><!-- .page-wrapper -->
-
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
+			
 
 <?php get_footer(); ?>
