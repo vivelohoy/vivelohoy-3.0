24fbe6994754d8a08c377ce0be11a4d229480d57
Lucio Villa <contact@luciovilla.com>
2014-08-01 15:52:55 -0500

    Added lower leaderboard ad in category page.

--- wp-content/themes/twentythirteen-child/category.php
+++ wp-content/themes/twentythirteen-child/category.php
@@ -36,6 +36,10 @@ get_header(); ?>
 			<?php get_template_part( 'content', 'none' ); ?>
 		<?php endif; ?>
 
+			<div id="bottomleaderboard-post">
+				<iframe id="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=%s;ord=%s" height="90" width="728" vspace="0" hspace="0" marginheight="0" marginwidth="0" align="center" frameborder="0" scrolling="no" src="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=http://www.vivelohoy.com/;ord=86950313"></iframe>
+			</div>
+
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
