a1605111ae6875e65e78ce9ccca739eaa587bf0c
Lucio Villa <contact@luciovilla.com>
2014-08-01 11:40:45 -0500

    Updated loop in author, category and search page (#118).

--- wp-content/themes/twentythirteen-child/search.php
+++ wp-content/themes/twentythirteen-child/search.php
@@ -11,7 +11,11 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-
+		
+			<div id="topleaderboard-post" class="topleaderboard-home">
+				<iframe id="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=%s;ord=%s" height="90" width="728" vspace="0" hspace="0" marginheight="0" marginwidth="0" align="center" frameborder="0" scrolling="no" src="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=http://www.vivelohoy.com/;ord=86950313"></iframe>
+			</div>
+		
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
@@ -20,7 +24,7 @@ get_header(); ?>
 			</header>
 
 			<?php /* The loop */ ?>
-			<?php include_once("custom-loop.php") ?>
+			<?php include_once("home-loop.php") ?>
 
 			<?php twentythirteen_paging_nav(); ?>
 
