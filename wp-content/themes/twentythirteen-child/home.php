bc78d71e15d4f2852277b163e82bed5f59fb3f90
Lucio Villa <contact@luciovilla.com>
2014-08-07 11:23:25 -0500

    Updated headline view.

--- wp-content/themes/twentythirteen-child/home.php
+++ wp-content/themes/twentythirteen-child/home.php
@@ -17,11 +17,19 @@ get_header(); ?>
 				<?php /* The loop */ ?>
 				<?php include_once("home-loop.php") ?>
 
-				<?php twentythirteen_paging_nav(); ?>
+				 <?php
+			        if (function_exists(custom_pagination)) {
+			          custom_pagination($custom_query->max_num_pages,"",$paged);
+			        }
+			     ?>
 
 			<?php else : ?>
 				<?php get_template_part( 'content', 'none' ); ?>
 			<?php endif; ?>
+
+			<div id="bottomleaderboard-post">
+        		<iframe id="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=%s;ord=%s" height="90" width="728" vspace="0" hspace="0" marginheight="0" marginwidth="0" align="center" frameborder="0" scrolling="no" src="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=http://www.vivelohoy.com/;ord=86950313"></iframe>
+      		</div>  
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
