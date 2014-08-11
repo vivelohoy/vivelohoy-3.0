e8d5485d62c7ee833a4f27a6fd055cfc4b63ef6a
Lucio Villa <contact@luciovilla.com>
2014-08-11 11:13:50 -0500

    Updated author, category, tag and minor issues.

--- wp-content/themes/twentythirteen-child/author.php
+++ wp-content/themes/twentythirteen-child/author.php
@@ -27,9 +27,9 @@ get_header(); ?>
 				the_post();
 			?>
 
-			<header class="archive-header" style="text-align: center; margin-top: 20px">	
+			<header class="archive-header" style="text-align: center; margin-top: 37px">	
 				<div style="display: inline-block">	
-					<div style="float: left; margin-right:10px"><?php echo get_avatar( get_the_author_meta('email'), '120' ); ?></div>	
+					<div class="hoy-avatar" style="float: left; margin-right:10px"><?php echo get_avatar( get_the_author_meta('email'), '120' ); ?></div>	
 					<div style="float: left; text-align: left; max-width: 915px;">
 						<h1 style="margin:0"><?php printf(get_the_author()); ?>
 						</h1>
@@ -63,7 +63,7 @@ get_header(); ?>
 					</div>
 				</div>	
 			</header>
-			<hr>
+			<hr style="width: 75%; margin: 0 auto">
 
 
 			<?php
@@ -75,21 +75,25 @@ get_header(); ?>
 				rewind_posts();
 			?>
 
-			<div style="overflow: hidden"><style type="text/css">.author-meta {padding-left: 0;}</style>			
+			<div class="author-page"><style type="text/css">.author-meta {padding-left: 0;}</style>			
 				<?php include_once("home-loop.php") ?>
 			</div> <!-- End div style="overflow: hidden;" -->
 
 
-			<?php twentythirteen_paging_nav(); ?>
+			<?php
+			        if (function_exists(custom_pagination)) {
+			          custom_pagination($custom_query->max_num_pages,"",$paged);
+			        }
+			?>
 
 			<?php else : // If not have_posts() for the whole author page ?>
 				<?php get_template_part( 'content', 'none' ); ?>
 			<?php endif; // End if ( have_posts() ) for the whole author page ?>
-			<!-- BOTTOM LEADERBOARD AD -->	
+			<!-- BOTTOM LEADERBOARD AD 	
 				<div id="bottomleaderboard-post">
 					<iframe id="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=%s;ord=%s" height="90" width="728" vspace="0" hspace="0" marginheight="0" marginwidth="0" align="center" frameborder="0" scrolling="no" src="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=http://www.vivelohoy.com/;ord=86950313"></iframe>
 				</div>
-			<!-- BOTTOM LEADERBOARD AD -->
+			BOTTOM LEADERBOARD AD -->
 			
 			
 
--- wp-content/themes/twentythirteen-child/category.php
+++ wp-content/themes/twentythirteen-child/category.php
@@ -12,7 +12,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+		<div id="content" class="site-content" role="main" style="max-width: 960px; margin: 0 auto">
 
 			<div id="topleaderboard-post" class="topleaderboard-home">
 				<iframe id="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=%s;ord=%s" height="90" width="728" vspace="0" hspace="0" marginheight="0" marginwidth="0" align="center" frameborder="0" scrolling="no" src="http://ad.doubleclick.net/adi/trb.vivelohoy2/hp;tile=1;ptype=sf;pos=1;sz=728x90;u=http://www.vivelohoy.com/;ord=86950313"></iframe>
@@ -30,7 +30,11 @@ get_header(); ?>
 			<?php /* The loop */ ?>
 			<?php include_once("home-loop.php") ?>
 			
-			<?php twentythirteen_paging_nav(); ?>
+			<?php
+			        if (function_exists(custom_pagination)) {
+			          custom_pagination($custom_query->max_num_pages,"",$paged);
+			        }
+			?>
 
 		<?php else : ?>
 			<?php get_template_part( 'content', 'none' ); ?>
