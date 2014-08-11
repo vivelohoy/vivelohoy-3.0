5f7ee631658a225d0c6305d782380a69cf74b22c
Lucio Villa <contact@luciovilla.com>
2014-08-11 11:13:50 -0500

    Updated author, category, tag and minor issues.

--- wp-content/themes/twentythirteen-child/tag.php
+++ wp-content/themes/twentythirteen-child/tag.php
@@ -14,12 +14,12 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+		<div id="content" class="site-content" role="main" style="max-width: 960px; margin: 0 auto">
 
 		<?php if ( have_posts() ) : ?>
 			<header class="archive-header">
 				<h1 class="archive-title"><?php printf( __( 'Tag Archives: %s', 'twentythirteen' ), single_tag_title( '', false ) ); ?></h1>
-
+				<hr>
 				<?php if ( tag_description() ) : // Show an optional tag description ?>
 				<div class="archive-meta"><?php echo tag_description(); ?></div>
 				<?php endif; ?>
@@ -29,7 +29,11 @@ get_header(); ?>
 			<?php include_once("home-loop.php") ?>
 
 
-			<?php twentythirteen_paging_nav(); ?>
+			<?php
+			        if (function_exists(custom_pagination)) {
+			          custom_pagination($custom_query->max_num_pages,"",$paged);
+			        }
+			?>
 
 		<?php else : ?>
 			<?php get_template_part( 'content', 'none' ); ?>
