3a64748defe4d7899321288a216dd6781bedda13
Lucio Villa <contact@luciovilla.com>
2014-08-14 13:57:24 -0500

    Updated 404 page.

--- wp-content/themes/twentythirteen-child/404.php
+++ wp-content/themes/twentythirteen-child/404.php
@@ -8,16 +8,22 @@
  */
 
 get_header(); ?>
-</div><div style="margin: 30px auto">
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/404.jpg" alt="Error Cuatro-Cero-Cuatro..." width="100%">
+</div>
+
+<div style="margin: 70px auto; max-width:860px">
+	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/405.jpg" alt="Error Cuatro-Cero-Cuatro..." width="100%">
 					
-				<div class="page-content">
+				<div style="text-align:center">
 					<h2 class="error404-search"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentythirteen-child' ); ?></h2>
-					<p style="margin: 0 0 5px 0"><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen-child' ); ?></p>
+					<p style="margin: 0 0 5px 0; padding:0 10px"><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen-child' ); ?></p>
+					<form role="search" method="get" class="search-form" action="http://vagrant.dev/">
+						<label>
+							<span class="screen-reader-text">Buscar:</span>
+							<input type="search" class="search-field" placeholder="Buscar…" value="" name="s" title="Buscar:">
+						</label>
+					</form>
 					
-						<?php get_search_form(); ?>
-					
-				</div><!-- .page-content -->
-			
+				</div>
+</div>			
 
 <?php get_footer(); ?>
