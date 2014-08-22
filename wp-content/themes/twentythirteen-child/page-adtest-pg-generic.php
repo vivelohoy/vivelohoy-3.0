859693a4ef074cadbfaa9b6b2c626874923ddac2
Nick Bennett <nbennett@vivelohoy.com>
2014-08-22 10:12:02 -0500

    Create page template to test photo gallery ads.

---
+++ wp-content/themes/twentythirteen-child/page-adtest-pg-generic.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Template Name: Thumbnail View
+ * 
+ */
+?>
+
+<?php get_header('adtest-pg-generic'); ?>
+
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main" style="max-width:960px">
+
+    <h1>Ad Test - PG Generic</h1>
+
+    <h2>AdSlot 1:</h2>
+    <!-- Beginning Async AdSlot 1 for Ad unit trb.vivelohoy2/fotogalerias  ### size: [[728,90]] -->
+    <!-- Adslot's refresh function: googletag.pubads().refresh([gptadslots[1]]) -->
+    <div id='div-gpt-ad-523701446549874316-1'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-523701446549874316-1'); });
+        </script>
+    </div>
+    <!-- End AdSlot 1 -->
+
+    <h2>AdSlot 2:</h2>
+    <!-- Beginning Async AdSlot 2 for Ad unit trb.vivelohoy2/fotogalerias  ### size: [[728,90]] -->
+    <!-- Adslot's refresh function: googletag.pubads().refresh([gptadslots[2]]) -->
+    <div id='div-gpt-ad-523701446549874316-2'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-523701446549874316-2'); });
+        </script>
+    </div>
+    <!-- End AdSlot 2 -->
+
+    <h2>AdSlot 3:</h2>
+    <!-- Beginning Async AdSlot 3 for Ad unit trb.vivelohoy2/fotogalerias  ### size: [[728,90]] -->
+    <!-- Adslot's refresh function: googletag.pubads().refresh([gptadslots[3]]) -->
+    <div id='div-gpt-ad-523701446549874316-3'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-523701446549874316-3'); });
+        </script>
+    </div>
+    <!-- End AdSlot 3 -->
+
+    <h2>AdSlot 4:</h2>
+    <!-- Beginning Async AdSlot 4 for Ad unit trb.vivelohoy2/fotogalerias  ### size: [[728,90]] -->
+    <!-- Adslot's refresh function: googletag.pubads().refresh([gptadslots[4]]) -->
+    <div id='div-gpt-ad-523701446549874316-4'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-523701446549874316-4'); });
+        </script>
+    </div>
+    <!-- End AdSlot 4 -->
+
+    <h2>AdSlot 5:</h2>
+    <!-- Beginning Async AdSlot 5 for Ad unit trb.vivelohoy2/fotogalerias  ### size: [[728,90]] -->
+    <!-- Adslot's refresh function: googletag.pubads().refresh([gptadslots[5]]) -->
+    <div id='div-gpt-ad-523701446549874316-5'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-523701446549874316-5'); });
+        </script>
+    </div>
+    <!-- End AdSlot 5 -->
+
+    <h2>AdSlot 6:</h2>
+    <!-- Beginning Async AdSlot 6 for Ad unit trb.vivelohoy2/fotogalerias  ### size: [[728,90]] -->
+    <!-- Adslot's refresh function: googletag.pubads().refresh([gptadslots[6]]) -->
+    <div id='div-gpt-ad-523701446549874316-6'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-523701446549874316-6'); });
+        </script>
+    </div>
+    <!-- End AdSlot 6 -->
+
+    <h2>AdSlot 7:</h2>
+    <!-- Beginning Async AdSlot 7 for Ad unit trb.vivelohoy2/fotogalerias  ### size: [[728,90]] -->
+    <!-- Adslot's refresh function: googletag.pubads().refresh([gptadslots[7]]) -->
+    <div id='div-gpt-ad-523701446549874316-7'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-523701446549874316-7'); });
+        </script>
+    </div>
+    <!-- End AdSlot 7 -->
+
+    <h2>AdSlot 8:</h2>
+    <!-- Beginning Async AdSlot 8 for Ad unit trb.vivelohoy2/fotogalerias  ### size: [[728,90]] -->
+    <!-- Adslot's refresh function: googletag.pubads().refresh([gptadslots[8]]) -->
+    <div id='div-gpt-ad-523701446549874316-8'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-523701446549874316-8'); });
+        </script>
+    </div>
+    <!-- End AdSlot 8 -->
+
+    <h2>AdSlot 9:</h2>
+    <!-- Beginning Async AdSlot 9 for Ad unit trb.vivelohoy2/fotogalerias  ### size: [[728,90]] -->
+    <!-- Adslot's refresh function: googletag.pubads().refresh([gptadslots[9]]) -->
+    <div id='div-gpt-ad-523701446549874316-9'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-523701446549874316-9'); });
+        </script>
+    </div>
+    <!-- End AdSlot 9 -->
+
+    <h2>AdSlot 10:</h2>
+    <!-- Beginning Async AdSlot 10 for Ad unit trb.vivelohoy2/fotogalerias  ### size: [[728,90]] -->
+    <!-- Adslot's refresh function: googletag.pubads().refresh([gptadslots[10]]) -->
+    <div id='div-gpt-ad-523701446549874316-10'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-523701446549874316-10'); });
+        </script>
+    </div>
+    <!-- End AdSlot 10 -->
+
+    </div><!-- #content -->
+	</div><!-- #primary -->
+
+	<?php wp_reset_postdata(); ?>
+
+<?php get_footer(); ?>
