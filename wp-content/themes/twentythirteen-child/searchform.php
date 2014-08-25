9fe559d73ece905687e7cbfa0296b3a8b3708b9b
Lucio Villa <contact@luciovilla.com>
2014-08-25 15:32:25 -0500

    Added new font icon and social media sharing box at story level.

--- wp-content/themes/twentythirteen-child/searchform.php
+++ wp-content/themes/twentythirteen-child/searchform.php
@@ -1,5 +1,6 @@
 <script type="text/javascript">
     function toggle_visibility(id) {
+    	event.preventDefault();
        var e = document.getElementById(id);
        if(e.style.display == 'block')
           e.style.display = 'none';
