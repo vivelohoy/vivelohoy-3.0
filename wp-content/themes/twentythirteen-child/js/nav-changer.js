c222b10f9a5e8933f1c0f28fa401beb354488b4c
Lucio Villa <contact@luciovilla.com>
2014-08-27 12:37:12 -0500

    Updated story level nav.

--- wp-content/themes/twentythirteen-child/js/nav-changer.js
+++ wp-content/themes/twentythirteen-child/js/nav-changer.js
@@ -3,18 +3,10 @@ $(document).scroll(function () {
     var y = $(this).scrollTop();
     if (y < 200) {
         $('.topmenu').fadeIn(0);
+        $('.bottomMenu').fadeOut(0);
     }else {
         $('.topmenu').fadeOut(0);
-    }
-
-});
-$(document).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > 200) {
         $('.bottomMenu').fadeIn(0);
-    } else {
-        $('.bottomMenu').fadeOut(0);
     }
-
 });
 })(jQuery);
