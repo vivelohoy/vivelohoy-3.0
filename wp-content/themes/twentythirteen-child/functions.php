12c49f267caa3ae212805ae1de3dbde283b6074e
Lucio Villa <contact@luciovilla.com>
2014-08-28 11:45:43 -0500

    Removed extra fields in author admin.

--- wp-content/themes/twentythirteen-child/functions.php
+++ wp-content/themes/twentythirteen-child/functions.php
@@ -52,11 +52,21 @@ function add_to_author_profile( $contactmethods ) {
 	$contactmethods['google_profile'] = 'Google Profile URL';
 	$contactmethods['twitter_profile'] = 'Twitter Profile URL';
 	$contactmethods['facebook_profile'] = 'Facebook Profile URL';
-	$contactmethods['linkedin_profile'] = 'Linkedin Profile URL';
 	
 	return $contactmethods;
 }
 add_filter( 'user_contactmethods', 'add_to_author_profile', 10, 1);
+
+function add_twitter_contactmethod( $contactmethods ) {
+  unset($contactmethods['aim']);
+  unset($contactmethods['jabber']);
+  unset($contactmethods['yim']);
+  unset($contactmethods['twitter']);
+  unset($contactmethods['googleplus']);
+  unset($contactmethods['facebook']);
+  return $contactmethods;
+}
+add_filter('user_contactmethods','add_twitter_contactmethod',10,1);
 
 /**
  * Overiding post_nav function
