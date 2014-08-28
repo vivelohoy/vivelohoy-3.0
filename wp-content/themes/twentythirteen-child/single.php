c34ec75287217b95ece6157fe0373401a592b5aa
Nick Bennett <nbennett@vivelohoy.com>
2014-08-28 10:35:31 -0500

    Fix bottom leaderboard ad not displaying in regular story post.

--- wp-content/themes/twentythirteen-child/single.php
+++ wp-content/themes/twentythirteen-child/single.php
@@ -39,7 +39,7 @@ get_header(); ?>
 				<?php 
 					global $regular_story_ad_tag_ids;
 					$category_string = get_category_string();
-					print_ad_tag($regular_story_ad_tag_ids[$category_string][2]);
+					print_ad_tag($regular_story_ad_tag_ids[$category_string][3]);
 				?>
 				</div>
 			<?php endif; // get_post_format() ?>
