aa5384ab5108bc3915a98f563132902e61360a58
Nick Bennett <nbennett@vivelohoy.com>
2014-09-03 16:39:05 -0500

    Fix get_category_string() to work when get_category_names() returns false.

--- wp-content/themes/twentythirteen-child/inc/ads.php
+++ wp-content/themes/twentythirteen-child/inc/ads.php
@@ -89,22 +89,25 @@ FUNCTIONS
 function get_category_string() {
     // get_category_names() is defined in inc/utils.php
     $current_categories = get_category_names();
-
-    if ( in_array( 'deportes', $current_categories ) ) {
-        return 'deportes';
-    } elseif ( in_array( 'entretenimiento', $current_categories ) ) {
-        return 'entretenimiento';
-    } elseif ( in_array( 'chicago', $current_categories ) ) {
-        return 'noticias-chicago';
-    } elseif ( in_array( 'eeuu', $current_categories ) ) {
-        return 'noticias-eeuu';
-    } elseif ( in_array( 'mundo', $current_categories ) ) {
-        return 'noticias-mundo';
-    } elseif ( in_array( 'opinion', $current_categories ) ) {
-        return 'opinion';
+    if( $current_categories ) {
+        if ( in_array( 'deportes', $current_categories ) ) {
+            return 'deportes';
+        } elseif ( in_array( 'entretenimiento', $current_categories ) ) {
+            return 'entretenimiento';
+        } elseif ( in_array( 'chicago', $current_categories ) ) {
+            return 'noticias-chicago';
+        } elseif ( in_array( 'eeuu', $current_categories ) ) {
+            return 'noticias-eeuu';
+        } elseif ( in_array( 'mundo', $current_categories ) ) {
+            return 'noticias-mundo';
+        } elseif ( in_array( 'opinion', $current_categories ) ) {
+            return 'opinion';
+        } else {
+            return 'default';
+        }
     } else {
         return 'default';
-    } 
+    }
 }
 
 function print_ad_tag($ad_tag_id) {
