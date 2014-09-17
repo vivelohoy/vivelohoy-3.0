295773996a5032638ef06ad5ae6df1d6300f5593
Nick Bennett <nbennett@vivelohoy.com>
2014-09-17 11:53:00 -0500

    Add per-image pageview counting for Google Analytics in galleries.

--- wp-content/themes/twentythirteen-child/js/gallery_analytics.js
+++ wp-content/themes/twentythirteen-child/js/gallery_analytics.js
@@ -1,10 +1,12 @@
 (function($) {
     $(document).ready(function() {
         function increment_pageview(photo_idx) {
-            // 's' is Omniture's object
+            // Omniture
             s.prop37 = "Photo " + photo_idx;
             s.events = "";
             s.t();
+            // Google Analytics
+            ga('send', 'pageview');
         }
         // Add a data attribute 'index' to each gallery item indicating which one it is
         // in numeric order from top to bottom, starting at 1.
