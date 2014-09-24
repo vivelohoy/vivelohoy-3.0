f78969009e7b2fa5d28252699ec55c52308220c1
Lucio Villa <contact@luciovilla.com>
2014-09-24 13:10:56 -0500

    Updated the gallery post format archive page.

---
+++ wp-content/themes/twentythirteen-child/taxonomy-post_format.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying Post Format pages
+ *
+ * Used to display archive-type pages for posts with a post format.
+ * If you'd like to further customize these Post Format views, you may create a
+ * new template file for each specific one.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header('home'); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main" style="max-width:960px">
+	
+			<?php global $ADS_ENABLED; if ( $ADS_ENABLED ) : ?>
+				<div id="topleaderboard-post" class="topleaderboard-home">
+					<?php 
+						global $ad_tag_ids;
+						print_ad_tag($ad_tag_ids["home page"]["leaderboard-top"]);
+					?>
+				</div>
+			<?php endif; // End if ( $ADS_ENABLED ) ?>
+					
+				<?php include('home-loop.php') ?>
+
+					 <?php
+				        if (function_exists(custom_pagination)) {
+				          custom_pagination($custom_query->max_num_pages,"",$paged);
+				        }
+				     ?>
+
+				
+			<?php global $ADS_ENABLED; if ( $ADS_ENABLED ) : ?>		     
+				<div id="bottomleaderboard-post">
+					<?php 
+						global $ad_tag_ids;
+						print_ad_tag($ad_tag_ids["home page"]["leaderboard-bottom"]);
+					?>
+		  		</div>
+		  		  
+		    <?php endif; // End if ( $ADS_ENABLED ) ?>
+	
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
