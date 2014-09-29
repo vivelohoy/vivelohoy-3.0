5f6cc2f75d9486950b2aa4cdf7628177532fa3d5
Nick Bennett <nbennett@vivelohoy.com>
2014-09-29 10:31:06 -0500

    Remove debug functions and unused files.

--- wp-content/themes/twentythirteen-child/inc/utils.php
+++ wp-content/themes/twentythirteen-child/inc/utils.php
@@ -14,34 +14,3 @@ function get_category_names() {
         return array();
     }
 }
-
-/*
-This is useful mainly for debugging purposes. Insert a <?php print_info(); ?> block into a page
-like single.php or category.php.
-*/
-function print_info() {
-    $c = get_category_names();
-    if ( $c ) {
-        echo "Categories:<br>";
-        echo "<pre>";
-        print_r($c);
-        echo "</pre>";
-    }
-    if ( is_single() ) {
-        $post_format = get_post_format();
-        if ( false === $post_format) {
-            $post_format = 'standard';
-        }
-        $post_type = get_post_type();
-
-        echo "<hr>Post Format: $post_format<br><hr>";
-
-        echo "Post Type: $post_type<br><hr>";
-    }
-}
-
-function hoy_var_dump($var) {
-    echo "<pre>";
-    var_dump($var);
-    echo "</pre>";
-}
