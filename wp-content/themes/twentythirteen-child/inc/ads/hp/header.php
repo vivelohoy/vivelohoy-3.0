bce717a0baa606de91576843ad96fa2f3bd8819d
Nick Bennett <nbennett@vivelohoy.com>
2014-09-29 14:19:25 -0500

    Add mobile ads to home page experimentally.

--- wp-content/themes/twentythirteen-child/inc/ads/hp/header.php
+++ wp-content/themes/twentythirteen-child/inc/ads/hp/header.php
@@ -24,17 +24,25 @@ if ($AD_TAG_DEV) {
 
     googletag.cmd.push(function() {
 
-        //Adslot 1 declaration
+        // Desktop Ads
         gptadslots[1]= googletag.defineSlot('/4011/trb.vivelohoy2/hp', [[728,90]],'desktop-ad-top-leaderboard').setTargeting('pos',['1']).addService(googletag.pubads());
 
-        //Adslot 2 declaration
         gptadslots[2]= googletag.defineSlot('/4011/trb.vivelohoy2/hp', [[728,90]],'desktop-ad-river-leaderboard-1').setTargeting('pos',['2']).addService(googletag.pubads());
 
-        //Adslot 3 declaration
         gptadslots[3]= googletag.defineSlot('/4011/trb.vivelohoy2/hp', [[728,90]],'desktop-ad-river-leaderboard-2').setTargeting('pos',['3']).addService(googletag.pubads());
 
-        //Adslot 4 declaration
         gptadslots[4]= googletag.defineSlot('/4011/trb.vivelohoy2/hp', [[728,90]],'desktop-ad-bottom-leaderboard').setTargeting('pos',['4']).addService(googletag.pubads());
+
+        // Mobile Ads
+        gptadslots[5] = googletag.defineSlot('/4011/trb.vivelohoy2', [[320,50]], 'mobile-ad-top-leaderboard').setTargeting('pos',['1']).addService(googletag.pubads());
+
+        gptadslots[6] = googletag.defineSlot('/4011/trb.vivelohoy2', [[320,50]], 'mobile-ad-river-leaderboard-1').setTargeting('pos',['2']).addService(googletag.pubads());
+
+        gptadslots[7] = googletag.defineSlot('/4011/trb.vivelohoy2', [[320,50]], 'mobile-ad-river-leaderboard-2').setTargeting('pos',['3']).addService(googletag.pubads());
+
+        gptadslots[8] = googletag.defineSlot('/4011/trb.vivelohoy2', [[320,50]], 'mobile-ad-bottom-leaderboard').setTargeting('pos',['4']).addService(googletag.pubads());
+
+
 
         googletag.pubads().setTargeting('ptype',['<?php echo $ptype; ?>']);
         googletag.pubads().enableAsyncRendering();
@@ -65,6 +73,24 @@ if ($AD_TAG_DEV) {
     </div>
     <!-- END - Desktop Ad Tags -->
     <!-- START - Mobile Ad Tags -->
+    <div class="loop-leaderboard mobile-ad adposition1">
+        <hr>
+        <div id="mobile-ad-river-leaderboard-1">
+            <script type="text/javascript">
+                googletag.cmd.push(function() { googletag.display("mobile-ad-river-leaderboard-1"); });
+            </script>
+        </div>
+        <hr>
+    </div>
+    <div class="loop-leaderboard mobile-ad adposition2">
+        <hr>
+        <div id="mobile-ad-river-leaderboard-2">
+            <script type="text/javascript">
+                googletag.cmd.push(function() { googletag.display("mobile-ad-river-leaderboard-2"); });
+            </script>
+        </div>
+        <hr>
+    </div>    
     <!-- END - Mobile Ad Tags -->
 </div>
 
