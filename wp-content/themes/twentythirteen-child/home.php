82630232243339729a9e6921cdedfbee053817f3
Nick Bennett <nbennett@vivelohoy.com>
2014-09-30 16:32:34 -0500

    Refactor to display ads responsively.

--- wp-content/themes/twentythirteen-child/home.php
+++ wp-content/themes/twentythirteen-child/home.php
@@ -9,45 +9,21 @@ get_header('home'); ?>
 		<div id="content" class="site-content" role="main" style="max-width:960px">
 	<?php global $ADS_ENABLED; if ( $ADS_ENABLED ) : ?>
 			<div id="topleaderboard-post" class="topleaderboard-home">
-				<div id="desktop-ad-top-leaderboard" class="desktop-ad">
-				    <script type='text/javascript'>
-				        googletag.cmd.push(function() { googletag.display("desktop-ad-top-leaderboard"); });
-				    </script>
-				</div>
-				<div id="mobile-ad-top-leaderboard" class="mobile-ad">
-				    <script type='text/javascript'>
-				        googletag.cmd.push(function() { googletag.display("mobile-ad-top-leaderboard"); });
-				    </script>
-				</div>  
-			</div>
-	<?php endif; // End if ( $ADS_ENABLED ) ?>
-			
-			
-				
+				<div id="desktop-ad-top-leaderboard" class="adslot desktop-ad" data-width="728" data-height="90" data-dfp="/4011/trb.vivelohoy2/hp" data-pos="1"></div> 
+      		</div> 
+	<?php endif; // End if ( $ADS_ENABLED ) ?>		
 				
 				<?php include('home-loop.php') ?>
-				
-
 
 				 <?php
 			        if (function_exists(custom_pagination)) {
 			          custom_pagination($custom_query->max_num_pages,"",$paged);
 			        }
 			     ?>
-
-			
-	<?php global $ADS_ENABLED; if ( $ADS_ENABLED ) : ?>		     
+		
+	<?php global $ADS_ENABLED; if ( $ADS_ENABLED ) : ?>	     
 			<div id="bottomleaderboard-post">
-				<div id="desktop-ad-bottom-leaderboard" class="desktop-ad">
-				    <script type='text/javascript'>
-				        googletag.cmd.push(function() { googletag.display("desktop-ad-bottom-leaderboard"); });
-				    </script>
-				</div>
-				<div id="mobile-ad-bottom-leaderboard" class="mobile-ad">
-				    <script type='text/javascript'>
-				        googletag.cmd.push(function() { googletag.display("mobile-ad-bottom-leaderboard"); });
-				    </script>
-				</div>  
+				<div id="desktop-ad-bottom-leaderboard" class="adslot desktop-ad" data-width="728" data-height="90" data-dfp="/4011/trb.vivelohoy2/hp" data-pos="4"></div>
       		</div>  
     <?php endif; // End if ( $ADS_ENABLED ) ?>
 		</div><!-- #content -->
