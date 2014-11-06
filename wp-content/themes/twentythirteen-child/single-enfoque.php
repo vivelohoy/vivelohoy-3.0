6e1750ec608baef7d93b8b479ca81214ccdba545
Lucio Villa <contact@luciovilla.com>
2014-11-06 17:09:37 -0600

    Updated Enfoque post.

--- wp-content/themes/twentythirteen-child/single-enfoque.php
+++ wp-content/themes/twentythirteen-child/single-enfoque.php
@@ -8,20 +8,15 @@ get_header('enfoque');
 
 ?>
 
+			<div id="primary" class="content-area" style="height:100%">
 
-			<div class="boxSep">
-    			<div class="imgLiquidFill imgLiquid">
-
-       				<?php echo wp_get_attachment_image( get_field('main_image'), 'full' ); ?>
-
-    				<!-- Adding blank link to overide Liquidimg a: style. Original code here: https://github.com/karacas/imgLiquid/blob/01bc41e8ac32c0e9730b59d75a5f53f373173b6d/js/imgLiquid.js#L164-L168 -->
-    				<div style="display:none"><a href="#"></a></div>
+				<div class="boxSep">
 
 					<header class="enfoque-header">
-						<div class="post-in-loop">
+						<div class="post-in-loop" style="margin:0 15px; width:initial">
 							<h1 class="enfoque-title"><?php echo get_the_title(); ?></h1>
 							<div class="enfoque-author-link">
-						        Por <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
+						        <?php _e( 'By ', 'gabo'); ?> <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
 						            <?php echo get_author_name( get_the_author_meta( 'ID' ) ); ?>
 						        </a>
 						    </div>
@@ -29,7 +24,7 @@ get_header('enfoque');
 						        <?php
 						            $category = get_the_category();
 						            if ( $category[0] ) { ?>
-						                <span class="category-en">en </span>
+						                <span class="category-en"><?php _e( 'in ', 'gabo'); ?> </span>
 						                <a href="<?php echo get_category_link( $category[0]->term_id ); ?>">
 						                    <?php echo $category[0]->cat_name; ?>
 						                </a><?php
@@ -44,47 +39,40 @@ get_header('enfoque');
 							</div>
 						</div>
 					</header>
+	    			<div class="imgLiquidFill imgLiquid">
 
-				</div>
-<!-- Disabling bouncing arrow for now.
-    			<div class="bar">
-	    			<div class="bounce" style="position: absolute; right: 0" id="bar">
-	        			<a href="#post-<?php the_ID(); ?>"><img width="100px" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/arrow.png" style="opacity: 0.5"></a>
-	        		</div>
-        		</div>
+	       				<?php echo wp_get_attachment_image( get_field('main_image'), 'full' ); ?>
 
--->
-    			<div style="position: absolute; width: 100%; text-align: right; padding-right: 10px;">
-    				<p style="color:#000; font-size:15px; font-family: 'Helvetica', Helvetica, Arial, 'Lucida Grande', sans-serif">Foto por <a href="<?php the_field('photo_link'); ?>" target="_blank"><?php the_field('photo_byline'); ?></a></p>
-    			</div>
+					</div>
+					<div style="width: 100%; text-align: right; padding-right: 10px;">
+    					<p style="color:#000; font-size:15px; font-family: 'Helvetica', Helvetica, Arial, 'Lucida Grande', sans-serif">
+    						Foto por <a href="<?php the_field('photo_link'); ?>" target="_blank"><?php the_field('photo_byline'); ?></a>
+    					</p>
+    				</div>
 
-			</div>
+    				<div id="content" class="site-content" role="main" style="text-align: left; padding: 0 10px">
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+							<div class="entry-content">
+							<?php the_field('article'); ?>
+							</div><!-- .entry-content -->
+
+						</article><!-- #post -->
+
+
+<?php global $ADS_ENABLED; if ( $ADS_ENABLED ) : ?>	
+			<!-- BOTTOM LEADERBOARD AD -->
+			<?php if ( 'gallery' !== get_post_format() ) : // Anything but a gallery post type ?>
+				<div id="bottom-leaderboard" class="adslot leaderboard" data-width="728" data-height="90" data-pos="3"></div> 
+			<?php endif; // get_post_format() ?>
+			<!-- BOTTOM LEADERBOARD AD -->
+<?php endif; // End if ( $ADS_ENABLED ) ?>
+
+			<script>
+			var is_gallery = <?php if ( 'gallery' === get_post_format() ) : ?>true<?php else: ?>false<?php endif; ?>;
+			</script>
+
+<?php get_footer('enfoque'); ?>
 
 
 
-			<div id="primary" class="content-area">
-				<div id="content" class="site-content" role="main">
-					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-						<div class="entry-content">
-						<?php the_field('article'); ?>
-						</div><!-- .entry-content -->
-
-					</article><!-- #post -->
-
-					<?php global $ADS_ENABLED; if ( $ADS_ENABLED ) : ?>	
-								<!-- BOTTOM LEADERBOARD AD -->
-								<?php if ( 'gallery' !== get_post_format() ) : // Anything but a gallery post type ?>
-									<div id="bottom-leaderboard" class="adslot leaderboard" data-width="728" data-height="90" data-pos="3"></div> 
-								<?php endif; // get_post_format() ?>
-								<!-- BOTTOM LEADERBOARD AD -->
-					<?php endif; // End if ( $ADS_ENABLED ) ?>
-
-					<script>
-					var is_gallery = <?php if ( 'gallery' === get_post_format() ) : ?>true<?php else: ?>false<?php endif; ?>;
-					</script>
-
-				</div><!-- #content -->
-			</div><!-- #primary -->
-
-<?php get_footer(); ?>
