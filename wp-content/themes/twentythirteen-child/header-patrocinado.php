534ea7935bd1e5e53d2043a2a1dc9119c83f6a0f
Lucio Villa <contact@luciovilla.com>
2014-11-20 15:48:02 -0600

    Added no follow meta tags to sponsored content posts.

--- wp-content/themes/twentythirteen-child/header-patrocinado.php
+++ wp-content/themes/twentythirteen-child/header-patrocinado.php
@@ -21,7 +21,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-
+	<meta name="googlebot-news" content="noindex,nofollow" />
 <?php
 	// get the current page url (used for rel canonical and open graph tags)
 	global $current_url;
