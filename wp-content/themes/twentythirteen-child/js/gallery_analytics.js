752cbdaf0168a4dcc1af62d5bd4b1fc8158ccdee
Nick Bennett <nbennett@vivelohoy.com>
2014-12-30 15:49:00 -0600

    Move analytics pageview increment function to be defined globally.

--- wp-content/themes/twentythirteen-child/js/gallery_analytics.js
+++ wp-content/themes/twentythirteen-child/js/gallery_analytics.js
@@ -1,13 +1,14 @@
+function increment_pageview_for_photo(photo_idx) {
+    // Omniture
+    s.prop37 = "Photo " + photo_idx;
+    s.events = "";
+    s.t();
+    // Google Analytics
+    ga('send', 'pageview');
+}
+
 (function($) {
     $(document).ready(function() {
-        function increment_pageview(photo_idx) {
-            // Omniture
-            s.prop37 = "Photo " + photo_idx;
-            s.events = "";
-            s.t();
-            // Google Analytics
-            ga('send', 'pageview');
-        }
         // Add a data attribute 'index' to each gallery item indicating which one it is
         // in numeric order from top to bottom, starting at 1.
         $('.gallery-item').each(function(index, item) {
@@ -15,8 +16,9 @@
             $(item).data({'index': index + 1});
         });
 
+        // When a gallery item scrolls fully into view, count a page view
         $('.gallery-item').waypoint(function () {
-            increment_pageview($(this).data('index'));
+            increment_pageview_for_photo($(this).data('index'));
         }, {
             triggerOnce: true,
             offset: 'bottom-in-view'
