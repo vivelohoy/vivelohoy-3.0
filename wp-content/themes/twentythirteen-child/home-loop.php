bb91ce01c873ea3b700235b4075a8131741241db
Lucio Villa <contact@luciovilla.com>
2015-01-12 15:48:22 -0600

    Updated front page video player.

--- wp-content/themes/twentythirteen-child/home-loop.php
+++ wp-content/themes/twentythirteen-child/home-loop.php
@@ -87,31 +87,3 @@ if ( 'gallery' === get_post_format() ) {
 </div>
 
 <?php endwhile; // End while ( have_posts() ) ?>
-
-<!-- Start Brightcove video player -->
-
-<div class="video-container">
-    <div class="video-title">
-        <span>VÍDEOS</span>
-      <!-- Commenting out button that links to video page  <button><span class="mas-videos-btn">MÁS VÍDEOS</span></button> -->
-    </div>
-    <script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>
-
-    <div class="brighcove-player">
-        <object id="myExperience" class="BrightcoveExperience">
-          <param name="bgcolor" value="#FFFFFF" />
-          <param name="width" value="860" />
-          <param name="height" value="460" />
-          <param name="playerID" value="3971228075001" />
-          <param name="playerKey" value="AQ~~,AAAB2Ejp1kE~,qYgZ7QVyRmC7WYNrwwfATzRsOr1tXGiH" />
-          <param name="isVid" value="true" />
-          <param name="isUI" value="true" />
-          <param name="dynamicStreaming" value="true" />
-        </object>
-    </div>
-
-    <script type="text/javascript">brightcove.createExperiences();</script>
-
-</div>
-
-<!-- End Brightcove video player -->
