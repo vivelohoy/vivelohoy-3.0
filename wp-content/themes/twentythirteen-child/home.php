a637103906881fc2c3e80ac6fb16240622fefa80
Lucio Villa <contact@luciovilla.com>
2015-01-12 15:48:22 -0600

    Updated front page video player.

--- wp-content/themes/twentythirteen-child/home.php
+++ wp-content/themes/twentythirteen-child/home.php
@@ -12,6 +12,33 @@ get_header('home'); ?>
 	<?php endif; // End if ( $ADS_ENABLED ) ?>
 
 	<?php include('home-loop.php') ?>
+	<!-- Start Brightcove video player -->
+
+	<div class="video-container">
+	    <div class="video-title">
+	        <span>VÍDEOS</span>
+	        <a href="<?php echo home_url() ?>/type/video"><button><span class="mas-videos-btn">MÁS VÍDEOS</span></button></a>
+	    </div>
+	    <script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>
+
+	    <div class="brighcove-player">
+	        <object id="myExperience" class="BrightcoveExperience">
+	          <param name="bgcolor" value="#FFFFFF" />
+	          <param name="width" value="860" />
+	          <param name="height" value="460" />
+	          <param name="playerID" value="3971228075001" />
+	          <param name="playerKey" value="AQ~~,AAAB2Ejp1kE~,qYgZ7QVyRmC7WYNrwwfATzRsOr1tXGiH" />
+	          <param name="isVid" value="true" />
+	          <param name="isUI" value="true" />
+	          <param name="dynamicStreaming" value="true" />
+	        </object>
+	    </div>
+
+	    <script type="text/javascript">brightcove.createExperiences();</script>
+
+	</div>
+
+	<!-- End Brightcove video player -->
 
 	<?php
         if (function_exists(custom_pagination)) {
