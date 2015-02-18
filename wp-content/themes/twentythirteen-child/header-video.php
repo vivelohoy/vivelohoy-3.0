5bae4a20e2506f34ddc760051082b2a1ecba6491
Nick Bennett <nbennett@vivelohoy.com>
2015-02-18 13:21:14 -0600

    Move common hamburger icon code out of headers into header-part-hamburgericon.php.

--- wp-content/themes/twentythirteen-child/header-video.php
+++ wp-content/themes/twentythirteen-child/header-video.php
@@ -42,18 +42,7 @@
 	<div id="page" class="hfeed site" style="background-color: #f5f5f5">
 		<header id="masthead" class="site-header" role="banner" style="background-color: #f5f5f5; box-shadow: 0px 1px 1px #C0C0C0;">
 			<div class="site-header-inner full-container" style="max-width:100%">
-				<button id="menu-toggle" class="toggle">
-					<svg version="1.1" class="menu-toggle-image" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-						 width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
-						<g id="menu">
-							<g>
-								<rect x="3" y="4" width="18" height="3"/>
-								<rect x="3" y="10" width="18" height="3"/>
-								<rect x="3" y="16" width="18" height="3"/>
-							</g>
-						</g>
-					</svg>
-				</button>
+				<?php require( 'header-part-hamburgericon.php' ); ?>
 				<div class="video-hoy-logo">
 					<a href="<?php echo home_url() ?>">
 						<img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/hoy-logo.png">
