2901032ac04989d1ea5b448fb9401ff25ea7b5d4
Nick Bennett <nbennett@vivelohoy.com>
2015-02-18 13:21:14 -0600

    Move common hamburger icon code out of headers into header-part-hamburgericon.php.

--- wp-content/themes/twentythirteen-child/header-home.php
+++ wp-content/themes/twentythirteen-child/header-home.php
@@ -35,18 +35,7 @@
 
 		<header id="masthead" class="site-header" role="banner">
 			<div class="site-header-inner full-container">
-				<button id="menu-toggle" class="toggle">
-					<svg version="1.1" class="menu-toggle-image" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-						 width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
-						<g id="menu">
-							<g>
-								<rect x="3" y="4" width="18" height="3"/>
-								<rect x="3" y="10" width="18" height="3"/>
-								<rect x="3" y="16" width="18" height="3"/>
-							</g>
-						</g>
-					</svg>
-				</button>
+				<?php require( 'header-part-hamburgericon.php' ); ?>
 				<div class="site-branding">
 					<a href="<?php echo home_url() ?>">
 						<img class="nav-logo" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/hoy-logo.png">
--- wp-content/themes/twentythirteen-child/header-patrocinado.php
+++ wp-content/themes/twentythirteen-child/header-patrocinado.php
@@ -34,18 +34,7 @@
 	<div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
 		<div class="full-container" style="margin: 0 auto; position: relative; width: 100%">
-			<button id="menu-toggle" class="toggle">
-				<svg version="1.1" class="menu-toggle-image" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-					 width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
-					<g id="menu">
-						<g>
-							<rect x="3" y="4" width="18" height="3"/>
-							<rect x="3" y="10" width="18" height="3"/>
-							<rect x="3" y="16" width="18" height="3"/>
-						</g>
-					</g>
-				</svg>
-			</button>
+			<?php require( 'header-part-hamburgericon.php' ); ?>
 			<div class="article-square-logo" style="padding: 0px 25px; display:block !important;">
 				<a style="float:left;border-bottom: none; padding: 5.5px" href="<?php echo home_url() ?>">
 					<img class="nav-logo" style="width: 35px; background: #F4F4F4; margin: 5px" src="<?php echo get_stylesheet_directory_uri();?>/images/square_logo.png">
