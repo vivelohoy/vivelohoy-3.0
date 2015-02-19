8fc902e80338c564825747788cde56b0a6f27fe8
Lucio Villa <contact@luciovilla.com>
2015-02-19 12:49:20 -0600

    Created new compartelo class for patrocinado.

--- wp-content/themes/twentythirteen-child/header-patrocinado.php
+++ wp-content/themes/twentythirteen-child/header-patrocinado.php
@@ -42,7 +42,19 @@
 				<div class="patrocinado-nav-title">
 					PATROCINADO
 				</div>
-				<?php require( 'header-part-compartelo.php' ); ?>
+                <div style="position: absolute; top: 10px; right: 0">
+                    <div class="compartelo_patrocinado">COMPÁRTELO: </div>
+                    <?php
+                    $facebook_share_link = "https://www.facebook.com/sharer/sharer.php?u=";
+                    $facebook_share_link .= urlencode(get_permalink());
+                    ?>
+                    <a class="twitter-share-link" href="" target="_blank" style="border-bottom: none">
+                        <span class="genericon genericon-twitter" style="color: #55acee; margin: 0; width: 35px"></span>
+                    </a>
+                    <a href="<?php echo $facebook_share_link; ?>" target="_blank" style="border-bottom: none">
+                        <span class="genericon genericon-facebook" style="margin-right: 0; color:#3b5998; width: 35px"></span>
+                    </a>
+                </div>
 			</div>
 		</div><!-- .site-header-inner -->
 	</header><!-- #masthead -->
