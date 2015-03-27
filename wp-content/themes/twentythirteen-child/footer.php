4288b9e0e244b3cbdd0798cf2f44780147924a5c
Lucio Villa <contact@luciovilla.com>
2015-03-27 14:05:55 -0700

    Implemented Simpli.Fi pixel in footer.

--- wp-content/themes/twentythirteen-child/footer.php
+++ wp-content/themes/twentythirteen-child/footer.php
@@ -23,5 +23,9 @@
 	</div><!-- #page -->
 	<?php render_omniture_tag(); ?>
 	<?php wp_footer(); ?>
+
+	<!-- Simpli.fi Pixel -->
+	<script async src="https://i.simpli.fi/dpx.js?cid=23231&action=100&segment=hoyllc&m=1&sifi_tuid=9736"></script>
+
 </body>
 </html>
