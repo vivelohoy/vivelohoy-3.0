188f0007dbe20b1b8390a2780e9482fd2a84bee5
Lucio Villa <contact@luciovilla.com>
2015-05-21 19:42:04 -0500

    Added event tracking analytics to video button.

--- wp-content/themes/twentythirteen-child/header-part-video.php
+++ wp-content/themes/twentythirteen-child/header-part-video.php
@@ -1,4 +1,4 @@
-				<a href="http://www.vivelohoy.com/video">
+				<a href="http://www.vivelohoy.com/video"  onClick="ga('send', 'event', { eventCategory: 'video button', eventAction: 'to video page'});">
 					<div class="video-btn-wrap">
 						<button class="video-btn">
 					    	<span style="font-family: arial,helvetica,sans-serif; font-size: 12px;">video</span>
