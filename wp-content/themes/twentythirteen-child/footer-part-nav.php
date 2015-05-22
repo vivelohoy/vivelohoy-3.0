1eb205234a3f1deafe758d17404687d42b7ea4ea
Lucio Villa <contact@luciovilla.com>
2015-05-22 14:02:36 -0500

    Removed actualizada from footer.

--- wp-content/themes/twentythirteen-child/footer-part-nav.php
+++ wp-content/themes/twentythirteen-child/footer-part-nav.php
@@ -3,7 +3,7 @@
 			© <?php echo date("Y"); ?> Hoy
             <a href="/about-vivelohoy/"> Acerca de nosotros</a>
             <a href="/advertise"> Advertise</a>
-            <a href="http://www.tribpub.com/terminos-de-servicio-principales/"> Términos de servicio (actualizada)</a>
- 			<a href="http://www.tribpub.com/politica-de-privacidad/"> Política de privacidad (actualizada)</a>
+            <a href="http://www.tribpub.com/terminos-de-servicio-principales/"> Términos de servicio</a>
+ 			<a href="http://www.tribpub.com/politica-de-privacidad/"> Política de privacidad</a>
 	</div>
 </footer>
--- wp-content/themes/twentythirteen-child/header-part-sidemenu.php
+++ wp-content/themes/twentythirteen-child/header-part-sidemenu.php
@@ -8,7 +8,7 @@
                     <div style="text-align:center; font-family:helvetica; font-weight:300; letter-spacing:0.5px; height:30px">Síganos en:</div>
                     <div style="height: 42px; text-align:center">
                         <a href="https://twitter.com/vivelohoy" target="_blank" style="padding-right:10px; border: none;"><span class="genericon genericon-twitter" style="color: #55acee"></span></a>
-                        <a href="https://www.facebook.com/HoyMedia" style="padding-right:20px; border: none;" target="_blank"><span class="genericon genericon-facebook" style="margin-right: 5px; color:#3b5998"></span></a>   
+                        <a href="https://www.facebook.com/HoyMedia" style="padding-right:20px; border: none;" target="_blank"><span class="genericon genericon-facebook" style="margin-right: 5px; color:#3b5998"></span></a>
                         <a href="http://instagram.com/vivelohoy" style="padding-right:10px;border: none;"><span class="genericon genericon-instagram" style="color: #3f729b"></span></a>
                         <a href="https://www.youtube.com/user/VIVELOHOY" style="border: none;"><span class="genericon genericon-youtube" style="color:#b31217;"></span></a>
                     </div>
@@ -17,8 +17,8 @@
                     ©<?php echo date("Y"); ?> Hoy<br>
                     <a href="/about-vivelohoy/"> Acerca de nosotros</a>
                     <a href="/advertise"> Advertise</a>
-                    <a href="http://www.tribpub.com/terminos-de-servicio-principales/"> Términos de servicio (actualizada)</a>
-                    <a href="http://www.tribpub.com/politica-de-privacidad/"> Política de privacidad (actualizada)</a>
+                    <a href="http://www.tribpub.com/terminos-de-servicio-principales/"> Términos de servicio</a>
+                    <a href="http://www.tribpub.com/politica-de-privacidad/"> Política de privacidad</a>
                 </div>
             </footer>
         </div>
